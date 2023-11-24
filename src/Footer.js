d422cafdd1ae54d6c489ed302c1ebcd729e008f0
Ravina <raniravina71996@gmail.com>
2023-11-24 19:28:57 +0200

    lab done

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; Ravina in 2023 React Lab</p>
+    </footer>
+  );
+}
+
+export default Footer;
